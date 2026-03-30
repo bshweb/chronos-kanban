32715605f84bb45540997d78eb1d17d49aafbb38
bshweb <boris.web@hotmail.com>
2026-03-30 21:37:14 +0200

    Frontend: Add plus-button for creating new Stages in the board prototype
    - Frontend: Refactor the board prototype for further dnd-logic

--- frontend/src/components/board/BoardView.vue
+++ frontend/src/components/board/BoardView.vue
@@ -1,6 +1,31 @@
 <script setup lang="ts">
-import GripVerticalIcon from '@/components/icons/GripVerticalIcon.vue'
+import GripIcon from '@/components/icons/GripIcon.vue'
 import BoardCreateArea from '@/components/board/BoardCreateArea.vue'
+import { ref } from 'vue'
+import PlusIcon from '@/components/icons/PlusIcon.vue'
+
+interface Stage {
+  id: string
+  title: string
+  position: bigint
+  tasks?: Task[]
+}
+
+interface Task {
+  id: string
+  title: string
+  description?: string
+  position: bigint
+}
+
+const stages = ref<Stage[]>([
+  { id: 'blah-id-0', title: 'Backlog', position: 0n },
+  { id: 'blah-id-1', title: 'To Do', position: 1n },
+  { id: 'blah-id-2', title: 'In Progress', position: 2n },
+  { id: 'blah-id-3', title: 'Review required', position: 3n },
+  { id: 'blah-id-4', title: 'Reviewed', position: 4n },
+  { id: 'blah-id-5', title: 'Done', position: 5n },
+])
 </script>
 
 <template>
@@ -15,16 +40,17 @@ import BoardCreateArea from '@/components/board/BoardCreateArea.vue'
       class="flex w-full flex-1 min-h-0 gap-3 overflow-x-auto overflow-y-hidden"
     >
       <div
-        class="board-page-stage flex flex-col h-full min-h-0 shrink-0 w-full max-w-100 rounded-xl bg-(--color-surface)"
-        v-for="(stage, index) in 40"
-        :key="index"
+        class="board-page-stage flex flex-col shrink-0 h-full min-h-0 w-full max-w-100 rounded-xl bg-(--color-surface) border border-(--color-border) shadow-2xs"
+        v-for="stage in stages"
+        :key="stage.id"
       >
         <div
           class="board-page-stage-title flex items-center shrink-0 rounded-xl p-2 mx-1 my-1 gap-x-2"
         >
-          <GripVerticalIcon class="size-5 shrink-0 cursor-grab active:cursor-grabbing" />
+          <GripIcon class="size-5 shrink-0 cursor-grab active:cursor-grabbing" />
           <span class="font-bold min-w-0 wrap-break-word"
-            >To Dofdsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa {{ stage }}</span
+            >Lorem ipsum testtttttttttttttttttttttttttttttttttttttttttttttttttt
+            {{ stage.title }}</span
           >
           <span>12</span>
         </div>
@@ -34,12 +60,17 @@ import BoardCreateArea from '@/components/board/BoardCreateArea.vue'
             v-for="(task, index) in 120"
             :key="index"
           >
-            <GripVerticalIcon class="size-5 shrink-0 cursor-grab active:cursor-grabbing" />
+            <GripIcon class="size-5 shrink-0 cursor-grab active:cursor-grabbing" />
             <span class="min-w-0 wrap-break-word"
               >Do thingfdsfdsafdsafdsafdsafasfsdafsdaffsadfasdfasdfsdafasdfsda {{ task }}</span
             >
           </div>
         </div>
+      </div>
+      <div
+        class="flex flex-col shrink-0 h-full min-h-0 w-full max-w-20 rounded-xl bg-(--color-surface) border border-(--color-border) shadow-2xs justify-center items-center hover:bg-(--color-bg) cursor-pointer"
+      >
+        <PlusIcon class="size-15" />
       </div>
     </div>
   </div>
