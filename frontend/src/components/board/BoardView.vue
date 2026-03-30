4841f74def5a6fbe91f0124ed35d67780de3ed60
bshweb <boris.web@hotmail.com>
2026-03-31 01:06:13 +0200

    Frontend: Implement drag-and-drop for the board prototype
    - Frontend: Install vue-draggable-plus and implement drag-and-drop on the board prototype.

--- frontend/src/components/board/BoardView.vue
+++ frontend/src/components/board/BoardView.vue
@@ -3,11 +3,12 @@ import GripIcon from '@/components/icons/GripIcon.vue'
 import BoardCreateArea from '@/components/board/BoardCreateArea.vue'
 import { ref } from 'vue'
 import PlusIcon from '@/components/icons/PlusIcon.vue'
+import { useDraggable } from 'vue-draggable-plus'
 
 interface Stage {
   id: string
   title: string
-  position: bigint
+  position: string // JSON.stringify() used in vue-draggable-plus can't serialize a BigInt value
   tasks?: Task[]
 }
 
@@ -19,13 +20,27 @@ interface Task {
 }
 
 const stages = ref<Stage[]>([
-  { id: 'blah-id-0', title: 'Backlog', position: 0n },
-  { id: 'blah-id-1', title: 'To Do', position: 1n },
-  { id: 'blah-id-2', title: 'In Progress', position: 2n },
-  { id: 'blah-id-3', title: 'Review required', position: 3n },
-  { id: 'blah-id-4', title: 'Reviewed', position: 4n },
-  { id: 'blah-id-5', title: 'Done', position: 5n },
+  { id: 'blah-id-0', title: 'Backlog', position: '0' },
+  { id: 'blah-id-1', title: 'To Do', position: '1' },
+  { id: 'blah-id-2', title: 'In Progress', position: '2' },
+  { id: 'blah-id-3', title: 'Review required', position: '3' },
+  { id: 'blah-id-4', title: 'Reviewed', position: '4' },
+  { id: 'blah-id-5', title: 'Done', position: '5' },
 ])
+
+const boardRefDnd = ref()
+useDraggable(boardRefDnd, stages, {
+  animation: 150,
+  draggable: '.board-page-stage',
+  direction: 'horizontal',
+  scroll: true,
+  scrollSensitivity: 300,
+  scrollSpeed: 16,
+  handle: '.handle-dnd-stages',
+  ghostClass: 'ghost',
+  forceFallback: true,
+  fallbackOnBody: true,
+})
 </script>
 
 <template>
@@ -37,6 +52,7 @@ const stages = ref<Stage[]>([
     <BoardCreateArea />
     <div
       id="board-page-board"
+      ref="boardRefDnd"
       class="flex w-full flex-1 min-h-0 gap-3 overflow-x-auto overflow-y-hidden"
     >
       <div
@@ -47,7 +63,7 @@ const stages = ref<Stage[]>([
         <div
           class="board-page-stage-title flex items-center shrink-0 rounded-xl p-2 mx-1 my-1 gap-x-2"
         >
-          <GripIcon class="size-5 shrink-0 cursor-grab active:cursor-grabbing" />
+          <GripIcon class="handle-dnd-stages size-5 shrink-0 cursor-grab active:cursor-grabbing" />
           <span class="font-bold min-w-0 wrap-break-word"
             >Lorem ipsum testtttttttttttttttttttttttttttttttttttttttttttttttttt
             {{ stage.title }}</span
@@ -76,4 +92,9 @@ const stages = ref<Stage[]>([
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.ghost {
+  opacity: 0.5;
+  background: var(--color-border);
+}
+</style>
