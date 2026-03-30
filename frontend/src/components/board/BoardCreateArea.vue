59ae313c8ff9458e00ecf9ffd463d6d94c401701
bshweb <boris.web@hotmail.com>
2026-03-30 12:27:34 +0200

    Extract board area into a separate component to improve code quality
    - Frontend: Add Lucide GripVertical icon as an SVG and remove the heroicons dependency in favor of SVG usage.
    - Frontend: Improve styling of board prototype elements.

---
+++ frontend/src/components/board/BoardCreateArea.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div id="board-page-main-create-area" class="flex flex-wrap justify-center gap-4">
+    <div id="board-page-create-task-area" class=""></div>
+    <span>add stage</span>
+  </div>
+</template>
+
+<style scoped></style>
--- frontend/src/components/board/BoardView.vue
+++ frontend/src/components/board/BoardView.vue
@@ -1,4 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import GripVerticalIcon from '@/components/icons/GripVerticalIcon.vue'
+import BoardCreateArea from '@/components/board/BoardCreateArea.vue'
+</script>
 
 <template>
   <div id="board-page-content" class="flex flex-col flex-1 min-h-0 w-full">
@@ -6,32 +9,35 @@
       <h1 class="font-bold">Kanban Board</h1>
       <span>Optimize your tasks</span>
     </div>
-    <div id="board-page-main-create-area" class="flex flex-wrap justify-center gap-4">
-      <span>add task</span>
-      <span>add stage</span>
-    </div>
+    <BoardCreateArea />
     <div
       id="board-page-board"
       class="flex w-full flex-1 min-h-0 gap-3 overflow-x-auto overflow-y-hidden"
     >
       <div
         class="board-page-stage flex flex-col h-full min-h-0 shrink-0 w-full max-w-100 rounded-xl bg-(--color-surface)"
-        v-for="(stage, index) in 4"
+        v-for="(stage, index) in 40"
         :key="index"
       >
-        <div class="board-page-stage-title flex items-center shrink-0 rounded-xl p-2 mx-2 my-1">
-          <span>⋮⋮</span>
-          <span>To Do {{ stage }}</span>
+        <div
+          class="board-page-stage-title flex items-center shrink-0 rounded-xl p-2 mx-1 my-1 gap-x-2"
+        >
+          <GripVerticalIcon class="size-5 shrink-0 cursor-grab active:cursor-grabbing" />
+          <span class="font-bold min-w-0 wrap-break-word"
+            >To Dofdsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa {{ stage }}</span
+          >
           <span>12</span>
         </div>
         <div class="board-page-stage-tasks flex flex-col flex-1 min-h-0 overflow-y-auto">
           <div
-            class="board-page-stage-task rounded-xl bg-(--color-surface-2) p-2 mx-2 my-1 wrap-break-word"
+            class="board-page-stage-task flex rounded-xl bg-(--color-surface-2) p-2 mx-1 my-1 items-center gap-x-2"
             v-for="(task, index) in 120"
             :key="index"
           >
-            <span>⋮⋮</span>
-            <span>Do thingfdsfdsafdsafdsafdsafasfsdafsdaffsadfasdfasdfsdafasdfsda {{ task }}</span>
+            <GripVerticalIcon class="size-5 shrink-0 cursor-grab active:cursor-grabbing" />
+            <span class="min-w-0 wrap-break-word"
+              >Do thingfdsfdsafdsafdsafdsafasfsdafsdaffsadfasdfasdfsdafasdfsda {{ task }}</span
+            >
           </div>
         </div>
       </div>
